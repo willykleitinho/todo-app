b072283b8e9842f3177cad31959af4af57401d88
willykleitinho <69064592+willykleitinho@users.noreply.github.com>
2021-11-03 16:19:33 -0300

    Done most of layout and start of JS

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 let todo = [
-  {task: "lol", completed: false},
-  {task: "task 2", completed: false},
-  {task: "task 3", completed: false}
-]
+  {task: 'lol', completed: false},
+  {task: 'task 2', completed: false},
+  {task: 'task 3', completed: false}
+];
+
+// TODO
+function toggleTheme() {
+  console.log('toggle theme.');
+}
+
+// TODO
+function renderTasks(tasks) {
+  console.log('updating tasks.');
+}
+
+// TODO
+function clearCompleted(tasks) {
+  console.log('clearing completed tasks.')
+}
+
+// TODO
+function saveTasks(tasks) {
+  console.log('saving tasks to local storage.');
+}
+
+// TODO
+function addTask(tasks) {
+  console.log('adding task.');
+}
+
+// TODO
+window.onload = () => {
+  console.log('page loaded.');
+  document.querySelector('.controls').addEventListener('click', (ev) => {
+    if (ev.target.tagName == 'BUTTON') {
+      console.log(buttonActions[ev.target.name]);
+    }
+  })
+}
+
+const buttonActions = {
+  'all-tasks': 'all-tasks',
+  'active-tasks': 'active-tasks',
+  'completed-tasks': 'completed-tasks',
+  'clear-completed': 'clear-completed',
+  'add-task': 'add-task',
+  'remove-task': 'remove-task'
+}
