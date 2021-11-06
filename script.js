4cc97895f3c39f915d0dd9951c0270e4bf340f69
Wellington <hanzo.hasashi.pmk@gmail.com>
2021-11-06 01:18:51 -0300

    Refactor: JS, HTML, CSS

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function ArrayToString(arr) {
-  let str = '';
-  for (let item of arr) {
-    str += `["${item[0]}", ${item[1]}]`;
-  }
-  return str;
-}
+
+// Task manager with localStorage saving
+// and Drag and Drop (only on desktop)
 
 class Tasks {
   constructor() {
@@ -16,84 +12,92 @@ class Tasks {
   addTask(title, completed = false) {
     this.__tasks.push(new Task(this.__id++, title, completed));
   }
-  
-  removeTask(id) {
-    this.__tasks.filter(task => task.id !== id);
-  }
 
+  // save tasks to the local storage in a format of 
+  // [String, Boolean] arrays
+  // eg. ["task 1", false]
   saveTasks() {
     let str = '';
     for (let task of this.__tasks) {
       str += `["${task.title}", ${task.completed}]`;
     }
-    // TODO
     window.localStorage.setItem('tasks', str);
   }
   
   removeCompleted() {
     this.__tasks = this.__tasks.filter(task => !task.completed);
+    this.renderTasks();
   }
 
+  // Filter and render tasks based on view mode
   renderTasks() {
     let tasksHTML = '';
     let position = 1;
+
     switch (this.__view) {
       case 'all':
         this.__tasks.forEach(task => {
           tasksHTML += task.renderTask(position++);
         });
         break;
+
       case 'active':
         this.__tasks.filter(task => !task.completed).forEach(task => {
           tasksHTML += task.renderTask(position++);
         });
         break;
+
       case 'completed':
         this.__tasks.filter(task => task.completed).forEach(task => {
           tasksHTML += task.renderTask(position++);
         });
         break;
     }
+
     this.saveTasks();
+
     document.getElementById('tasks-list').innerHTML = tasksHTML;
     document.getElementById('items-left').innerText = `${this.__tasks.filter(task => !task.completed).length} items left`;
 
-    document.querySelectorAll('[draggable="true"').forEach(item => {
-      item.removeEventListener('dragstart', dragStart);
-      item.removeEventListener('dragend', dragEnd);
-      item.addEventListener('dragstart', dragStart);
-      item.addEventListener('dragend', dragEnd);
+    // Add drag and drop event listeners
+    document.querySelectorAll('[draggable="true"').forEach(task => {
+      task.addEventListener('dragstart', dragStart);
+      task.addEventListener('dragend', dragEnd);
     });
   }
 
   changeView(view) {
     document.querySelector(`[data-view="${view}"]`).className = 'selected';
     document.querySelector(`[data-view="${this.__view}"]`).className = '';
+
     this.__view = view;
+    this.renderTasks();
   }
 
   updateTask(id) {
-    this.__tasks.find(task => task.id == id).updateTask();
+    this.__tasks.find(task => task.id == id).switchState();
+    this.renderTasks();
   }
 
   removeTask(id) {
     const index = this.__tasks.findIndex(task => task.id == id);
+
     if (index > -1) {
       this.__tasks.splice(index, 1);
     }
+
+    this.renderTasks();
   }
 
+  // Save in localStorage the tasks that where changed
+  // using the drag and drop and render them again
   updatePositions() {
-    if (!document.getElementById('tasks-list').childElementCount) {
-      console.log('returning');
-      return;
-    }
-
-    const currentPosition = [];
     const newTasks = [];
+
     document.querySelectorAll('.task').forEach(item => {
       newTasks.push(this.__tasks.find(task => task.id == item.dataset.id));
     });
+
     this.__tasks = newTasks;
     this.renderTasks();
   }
@@ -111,12 +115,14 @@ class Task {
       <li class="${this.__completed ? "task done" : "task"}" data-position="${position}" data-id="${this.__id}" draggable="true">
         <button class="checkmark" name="complete-task"}"></button>
         <span class="title">${this.__title}</span>
-        <button class="remove-task cross" name="delete-task"></button>
+        <button class="remove-task-button" name="delete-task"></button>
       </li>
     `;
   }
 
-  updateTask() {
+  // If task is completed, mark it as
+  // uncompleted, and vice versa
+  switchState() {
     this.__completed = !this.__completed;
   }
 
@@ -133,7 +139,6 @@ class Task {
   }
 }
 
-// TODO
 function toggleTheme() {
   if (document.body.className) {
     document.body.className = '';
@@ -142,12 +147,11 @@ function toggleTheme() {
   }
 }
 
-// TODO
+// Init Task Manager
 let todos = new Tasks();
 
 function updateView(elem) {
   todos.changeView(elem.dataset.view);
-  todos.renderTasks();
 }
 
 const buttonActions = {
@@ -156,36 +160,39 @@ const buttonActions = {
   'completed-tasks': updateView,
   'clear-completed': () => {
     todos.removeCompleted();
-    todos.renderTasks();
   },
-  'add-task': 'add-task',
   'delete-task': (elem) => {
     todos.removeTask(elem.parentElement.dataset.id);
-    todos.renderTasks();
   },
   'complete-task': (elem) => {
     todos.updateTask(elem.parentElement.dataset.id);
-    todos.renderTasks();
   }
 }
 
+// Add event listeners when page loads
 window.onload = () => {
-  console.log('page loaded.');
   document.querySelector('.tasks').addEventListener('click', (ev) => {
     if (ev.target.tagName == 'BUTTON') {
       buttonActions[ev.target.name](ev.target);
     }
   });
 
-  document.querySelector('form').addEventListener('submit', (ev) => {
+  document.getElementById('add-task-form').addEventListener('submit', (ev) => {
     ev.preventDefault();
-    const input = ev.target.firstElementChild;
-    todos.addTask(input.value);
-    todos.renderTasks();
-    input.value = '';
+
+    const input = ev.target.querySelector('#add-task-input');
+
+    if (input.value) {
+      todos.addTask(input.value.slice(0, 30));
+      todos.renderTasks();
+
+      input.value = '';
+    }
   });
 
+  // Get tasks from local storage and load them
   const tasks = window.localStorage.getItem('tasks');
+
   if (tasks) {
     let bool = false;
     let taskName = '';
@@ -201,19 +208,21 @@ window.onload = () => {
         bool = true;
       }
     }
+    todos.renderTasks();
   }
-  todos.renderTasks();
-  
   
   const tasksList = document.getElementById('tasks-list');
+
+  // Drag and drop (while dragging inside tasks list)
   tasksList.addEventListener('dragover', function (ev) {
     ev.preventDefault();
-    // offY = ev.pageY - this.offsetTop;
-    // console.log(offY);
-    const underneathElem = document.elementFromPoint(ev.pageX, ev.pageY)
+    
+    const underneathElem = document.elementFromPoint(ev.pageX, ev.pageY);
     const middle = underneathElem.offsetTop + underneathElem.offsetHeight / 2;
+
     if (underneathElem.tagName == 'LI') {
       if (ev.pageY > middle) {
+        // if underneathElem (element under mouse pointer) has a sibling
         if (sibling = underneathElem.nextElementSibling) {
           this.insertBefore(dropzoneIndicator, underneathElem.nextElementSibling);
         } else {
@@ -227,49 +236,53 @@ window.onload = () => {
 
 }
 
-// drag and drop functionality
-const tasksList = document.getElementById('tasks-list');
-const dropzoneIndicator = document.createElement('li');
-dropzoneIndicator.innerText = "Here is my new place";
-dropzoneIndicator.classList.add('dropzone-indicator');
+// Drag and drop functionality
+
 /* 
   drag events:
 
-  dragstart       started dragging
-  drag            is dragging
-  dragend         stopped dragging
+    dragstart     started dragging
+    dragend       stopped dragging
 
-  dragenter       dropzone enter
-  dragover        dropzone is dragging over
-  dragleave       dropzone exit
-  drop            dropzone dropped
+    dragover      dragging over dropzone
 */
+
+// Dropzone indicator element
+const dropzoneIndicator = document.createElement('li');
+dropzoneIndicator.innerText = "Here is my new place";
+dropzoneIndicator.classList.add('dropzone-indicator');
+
+// Temp reservoir for tasks list info
 const tasksListInfo = {
   height: undefined,
   children: undefined,
-  taskHeight: undefined
+  taskElemHeight: undefined
 };
 
+
+// dragstart event handler
 function dragStart(ev) {
   const tasksList = document.getElementById('tasks-list');
-  if (tasksList.childElementCount < 2) {
-    console.log('false');
-    return false;
-  }
+
   tasksListInfo.height = tasksList.offsetHeight;
   tasksListInfo.children = tasksList.children.length;
-  tasksListInfo.taskHeight = Math.ceil(tasksList.offsetHeight / tasksList.children.length)
-  this.style.display = 'none';
+  tasksListInfo.taskElemHeight = Math.ceil(tasksList.offsetHeight / tasksList.children.length);
+
+  // this: dragged element
   tasksList.insertBefore(dropzoneIndicator, this);
+  this.style.display = 'none';
 }
 
+
+// dragend event handler
 function dragEnd(ev) {
   const tasksList = document.getElementById('tasks-list');
-  console.log('stopped');
+
+  // this: dragged element
   tasksList.insertBefore(this, dropzoneIndicator);
   tasksList.removeChild(dropzoneIndicator);
+
   this.style.display = 'flex';
+
   todos.updatePositions();
 }
-
-
